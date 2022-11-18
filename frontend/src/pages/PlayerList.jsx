b58405cb36b7b498635d219ee2e19e6ce26d79fe
FPLTomeczek <tomaszs18m@gmail.com>
2022-11-18 15:58:37 +0100

    mui_table

--- frontend/src/pages/PlayerList.jsx
+++ frontend/src/pages/PlayerList.jsx
@@ -1,6 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
-import './../css/PlayerList.css'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
 
 function PlayerList() {
 
@@ -12,6 +19,8 @@ function PlayerList() {
 const [data, setData] = useState(null)
 const [loading, setLoading] = useState(true)
 const [error, setError] = useState(null)
+const [order, setOrder] = useState('asc')
+const header = ['Name', 'Goals', 'Assists', 'Points', 'Team']
 
 // useEffect(() => {
 //     fetch(`https://api.github.com/users/eunit99/repos`)
@@ -44,34 +53,50 @@ useEffect(() => {
     };
     getData();
   }, [loading]);
+
+  // const handleSort = (event) =>{
+  //   if(order === 'asc'){
+  //     setOrder('desc')
+  //   }
+  //   else if(order === 'desc'){
+  //     setOrder('asc')
+  //   }
+  // }
+
   return (
     <div className='PlayerList'>
         {loading && <p>Loading...</p>}
         {!loading && <p>Fetched data</p>}
-        <div className='PlayerListTable'>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Goals</th>
-          <th>Assists</th>
-          <th>Points</th>
-          <th>Team</th>
-        </tr>
-        {data.map((val, key) =>{
-          return(
-            <tr key={key}>
-              <td>{val.web_name}</td>
-              <td>{val.goals_scored}</td>
-              <td>{val.assists}</td>
-              <td>{val.total_points}</td>
-              <td>{val.team_code}</td>
-            </tr>
-          )
-        })}
-      </table>
-      </div>
+        <div>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="right" sortDirection={order}>Goals
+                  </TableCell>
+                  <TableCell align="right">Assists</TableCell>
+                  <TableCell align="right">Points</TableCell>
+                  <TableCell align="right">Team</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data && data.map((player, index) => (
+                  <TableRow
+                    key={index}
+                  >
+                    <TableCell>{player.web_name}</TableCell>
+                    <TableCell align="right">{player.goals_scored}</TableCell>
+                    <TableCell align="right">{player.assists}</TableCell>
+                    <TableCell align="right">{player.total_points}</TableCell>
+                    <TableCell align="right">{player.team_code}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
     </div>
-    
   )
 }
 
